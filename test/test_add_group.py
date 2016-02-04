a94d884f1a0f2a8f65bf9515eb35459669ba360a
gena701@walla.com <gena701@walla.com>
2016-02-04 10:20:08 +0200

    Созданы пакеты для разных зон ответственности (тест добавления группы)
    Созданы пакеты для разных зон ответственности (тест добавления группы)

---
+++ test/test_add_group.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from fixture.application import Application
+from model.group import Group
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="Group2", header="Test Group2", footer="Test Group2"))
+    app.logout()
+
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
